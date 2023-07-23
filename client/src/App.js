24851ee4059f59198ceb8d5156192af53bca49e7
Admin <Netamamann@gmail.com>
2023-07-23 18:57:45 +0300

    clean the code

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,11 @@ import TodoList from './TodoList';
 
 function App() {
   return (
-    <>
     <TodoList />
-    </>
-
   );
 }
 
 export default App;
-// import Todo from './TodoList';
-// import { Container, Typography } from '@mui/material';
 
-// function App() {
-//   return (
-//     <Container maxWidth="sm">
-//       <Typography variant="h4" component="h1" align="center" gutterBottom>
-//         To-Do List
-//       </Typography>
-//       <Todo />
-//     </Container>
-//   );
-// }
-
-// export default App;
 
 
